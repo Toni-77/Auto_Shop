0c6f7b53adc8b38f40f412b33f50ecd010907b29
Elton Dede <157653261+Toni-77@users.noreply.github.com>
2026-03-11 11:25:55 -0500

    updated env variables for netlify

--- firebase.js
+++ firebase.js
@@ -1,21 +1,41 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// 1. ADD THIS IMPORT FOR THE DATABASE
-import { getFirestore } from "firebase/firestore"; 
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // 1. ADD THIS IMPORT FOR THE DATABASE
+// import { getFirestore } from "firebase/firestore"; 
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,        
+//   projectId: "auto-shop-47ff0",
+//   storageBucket: "auto-shop-47ff0.firebasestorage.app",
+//   messagingSenderId: "191697520508",
+//   appId: "1:191697520508:web:029ad75dd6a0f48bc606f9",
+//   measurementId: "G-B00YLLQMMY"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// // 2. INITIALIZE AND EXPORT THE DATABASE
+// export const db = getFirestore(app); 
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,        
-  projectId: "auto-shop-47ff0",
-  storageBucket: "auto-shop-47ff0.firebasestorage.app",
-  messagingSenderId: "191697520508",
-  appId: "1:191697520508:web:029ad75dd6a0f48bc606f9",
-  measurementId: "G-B00YLLQMMY"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: "auto-shop-v2",
+  storageBucket: "auto-shop-v2.firebasestorage.app",
+  messagingSenderId: "344660926490",
+  appId: "1:344660926490:web:c8afbf0ddb7e745f4271a3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// 2. INITIALIZE AND EXPORT THE DATABASE
-export const db = getFirestore(app); 
